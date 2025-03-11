b243f021771cdae51c21e8dcb356834a89fd948e
Matheusdfr93 <matheusdfr93@gmail.com>
2025-03-11 16:44:08 -0300

    fix(models): edit class name

--- app/models.py
+++ app/models.py
@@ -1,23 +1,25 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship,backref
+from sqlalchemy.orm import relationship
 from database import Base
 
-class User(Base):
+class Player(Base):  
     __tablename__ = "players"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     id_position = Column(Integer, ForeignKey("positions.id"), nullable=False)
-    age = Column(Integer)
-    is_base = Column(Boolean)
-    is_loaned = Column(Boolean)
+    age = Column(Integer, nullable=False)
+    is_base = Column(Boolean, default=False)
+    is_loaned = Column(Boolean, default=False)
+
     position = relationship("Position", back_populates="players")
 
 
 class Position(Base):
     __tablename__ = "positions"
 
-    id = Column(Integer, primary_key = True)
-    position_name = Column(String, unique = True)
-    cd_position = Column(String, unique=True)    
+    id = Column(Integer, primary_key=True)
+    position_name = Column(String, unique=True, nullable=False)
+    cd_position = Column(String, unique=True, nullable=False)    
+
     players = relationship("Player", back_populates="position")
